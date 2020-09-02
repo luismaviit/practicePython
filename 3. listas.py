5ec877bd1483ed7a61ee0ba38a09ff31fcc63bd5
Luis Viloria Iturriago <luismaviit@gmail.com>
2020-09-02 16:25:12 -0500

    clase 2, sept 2 del 2020

---
+++ 3. listas.py
@@ -0,0 +1,18 @@
+listas = [5,9,"luis",-6,"estudiante"]
+print (listas[0])
+print (listas[-1])
+numeros = [1,2,3,4,9,6,7,8,9]
+print (numeros)
+print (numeros[0:])
+numeros[4]=5
+print (numeros)
+numeros.append(10)
+print (numeros)
+primero=[1,2,3]
+segundo=[4,5,6]
+tercero=[7,8,9]
+cuarto=[10,11,12]
+anidadas=[primero,segundo,tercero,cuarto]
+print(anidadas)
+print (anidadas[0])
+print (anidadas[0][2])
---
+++ 5 .operadores logicos.py
@@ -0,0 +1,21 @@
+#operadores logicos son:
+#NOT
+#AND
+#OR
+print(2+4==7)
+print (not True)
+print (not False == True)
+print (False and False)
+print (True and True)
+print (True and False)
+num = 10
+print ('num = 10')
+print ('num > 5 and num < 15')
+print (num > 5 and num < 15)
+#OR o disyuncion logica, con que se cumpla una de las dos siempre será verdadero
+print ('num > 5 or num < 15')
+print (num > 5 or num < 15)
+
+name = 'Luis'
+print (name[0] == "l")
+
